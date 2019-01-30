7282b989e59bd6691a1af16faa54f7783d461bb6
Ivan Andonov <ivan.andonovv@hotmail.com>
2019-01-31 00:00:04 +0100

    adding search options

--- TravelAgency/TravelAgency/Controllers/HomeController.cs
+++ TravelAgency/TravelAgency/Controllers/HomeController.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TravelAgency.Models.TravelAgency;
 
 namespace TravelAgency.Controllers
 {
     public class HomeController : Controller
     {
+        private VacationDb db = new VacationDb();
+
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult VacationSearch(string search)
+        {
+            return View(db.Vacations.Where(x => (x.City + ", " + x.Country).Contains(search) || search == null));
         }
 
     }
