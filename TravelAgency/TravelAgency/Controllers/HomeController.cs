63b53d6a7fd1ac9f6783c0462e4f76580da48b58
Ivan Andonov <ivan.andonovv@hotmail.com>
2019-01-31 17:15:30 +0100

    minor fixes and adding the detailed view interface of selected vacations

--- TravelAgency/TravelAgency/Controllers/HomeController.cs
+++ TravelAgency/TravelAgency/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using TravelAgency.Models.TravelAgency;
@@ -23,7 +24,21 @@ namespace TravelAgency.Controllers
 
         public ActionResult VacationSearchPartial(string search)
         {
-            return PartialView(db.Vacations.Where(x => (x.City + ", " + x.Country).Contains(search) || search == null));
+            return PartialView(db.Vacations.Where(x => (x.City + ", " + x.Country).Contains(search) || search == null).ToList());
+        }
+
+        public ActionResult VacationDetails(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Vacation vacation = db.Vacations.Find(id);
+            if (vacation == null)
+            {
+                return HttpNotFound();
+            }
+            return View(vacation);
         }
 
     }
